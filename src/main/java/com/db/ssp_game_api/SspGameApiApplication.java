5c9ad2ed5bf7441a50c2c7405b3b82de82202a66
Diego Barahona <diegoba29@gmail.com>
2025-10-22 22:26:05 +0200

    chore(boostrap-app): init springboot api

---
+++ src/main/java/com/db/ssp_game_api/SspGameApiApplication.java
@@ -0,0 +1,13 @@
+package com.db.ssp_game_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SspGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SspGameApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/ssp_game_api/SspGameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.ssp_game_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SspGameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
