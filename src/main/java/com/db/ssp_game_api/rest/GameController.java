f955b2c16508df0402d18502e3429bcdc0c29b7d
Diego Barahona <diegoba29@gmail.com>
2025-10-24 20:40:18 +0200

    feat(swagger): add swagger and api docs

--- src/main/java/com/db/ssp_game_api/rest/GameController.java
+++ src/main/java/com/db/ssp_game_api/rest/GameController.java
@@ -4,6 +4,9 @@ package com.db.ssp_game_api.rest;
 import com.db.ssp_game_api.application.service.GameService;
 import com.db.ssp_game_api.domain.model.GameResult;
 import com.db.ssp_game_api.rest.model.PlayRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameController {
     private final GameService gameService;
 
+    @Operation(summary = "Play stone, paper, scissors")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Game result"),
+            @ApiResponse(responseCode = "400", description = "Invalid request")
+    })
     @PostMapping("/play")
     public ResponseEntity<GameResult> play(@RequestBody PlayRequest playRequest){
         return new ResponseEntity<>(gameService.play(playRequest.getPlayerMove(),playRequest.getLevel()), HttpStatus.OK);
