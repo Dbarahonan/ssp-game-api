f38c748384beec4a6363f1b7132a12d2fe42b2f3
Diego Barahona <diegoba29@gmail.com>
2025-10-29 19:01:39 +0100

    fix(global-exception): change controller advice annotation

--- src/main/java/com/db/ssp_game_api/rest/GlobalExceptionHandler.java
+++ src/main/java/com/db/ssp_game_api/rest/GlobalExceptionHandler.java
@@ -5,10 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // Handles validation exceptions from the request body, for example null values.
